b748ae1b076af3011044ee62360b54d5c4c09021
Vladislav Bakin <vladislav@bakin.me>
2018-06-27 11:52:08 +0300

    Update 404 page and add url route to view this page in debug mode

--- eightfit/urls.py
+++ eightfit/urls.py
@@ -11,6 +11,14 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from django.views.defaults import page_not_found
+
+    urlpatterns = [
+                      url(r'^_/404/$', page_not_found,
+                          kwargs={'exception': Exception()}),
+                  ] + urlpatterns
+
+if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
